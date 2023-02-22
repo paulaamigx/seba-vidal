8786d83ac3d637d8613749372ad6de6553f3b32f
pauamigx <paulaamigo22@gmail.com>
2023-02-22 12:45:48 -0300

    chore: next for gh pages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,24 @@ const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
-  }
+  },
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
 }
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 module.exports = nextConfig
