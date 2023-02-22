3a52ec7f98e37c3390a4c4ddb892aa68058edbc9
pauamigx <paulaamigo22@gmail.com>
2023-02-22 12:58:59 -0300

    chore: original next config

--- next.config.js
+++ next.config.js
@@ -4,23 +4,5 @@ const nextConfig = {
   compiler: {
     styledComponents: true
   },
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: 'imgix',
-    path: 'the "domain" of your Imigix source',
-  },
-}
-
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
 }
 module.exports = nextConfig
